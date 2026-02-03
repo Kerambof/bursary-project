4380b25fcd2c94a688372a21e51bce92f832a321
Kerambof <kerambofrancis7@gmail.com>
2026-02-03 16:24:06 +0300

    Updated admin: constituency admins see only their constituency applications

--- bursary/admin.py
+++ bursary/admin.py
@@ -4,39 +4,50 @@ from .models import Application, County, Constituency, LevelOfStudy, Constituenc
 # =========================
 # Application Admin
 # =========================
-
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'admission_number', 'constituency', 'county', 'level_of_study', 'created_at')
+    list_filter = ('county', 'constituency', 'level_of_study')
+    search_fields = ('full_name', 'admission_number', 'school', 'course', 'phone')
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
+        # Superuser sees all applications
         if request.user.is_superuser:
             return qs
+        # Constituency admin sees only applications in their constituency
         try:
             constituency_admin = ConstituencyAdmin.objects.get(user=request.user)
             return qs.filter(constituency=constituency_admin.constituency)
         except ConstituencyAdmin.DoesNotExist:
-            return qs.none()
-
+            return qs.none()  # Staff without constituency sees nothing
 
 # =========================
 # ConstituencyAdmin Admin
 # =========================
-
 class ConstituencyAdminAdmin(admin.ModelAdmin):
     list_display = ('user', 'constituency')
-    # Only superusers can manage constituency admins
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.none()
+    search_fields = ('user__username', 'user__email', 'constituency__name')
+    list_filter = ('constituency',)
 
+    # Optional: only superuser can add/edit constituency admins
+    def has_module_permission(self, request):
+        return request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
 
 # =========================
 # Register models
 # =========================
-
 admin.site.register(Application, ApplicationAdmin)
 admin.site.register(County)
 admin.site.register(Constituency)
