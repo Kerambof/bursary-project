6e83b6946c7976d10c25b15f1f6f6e836eb9f988
Kerambof <kerambofrancis7@gmail.com>
2026-09-05 15:27:16 +0300

    Fix Bursalite Render host configuration

--- config/settings.py
+++ config/settings.py
@@ -129,8 +129,10 @@ SESSION_SAVE_EVERY_REQUEST = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
 
 EMAIL_HOST_USER = 'me.bursary@gmail.com'
 EMAIL_HOST_PASSWORD = 'wxej ahxo ywgs fgwi'
